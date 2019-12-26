db88180d197d382a5110a86fe39539345466caf6
Pedro Pinto <pedropinto24@hotmail.com>
2019-12-26 15:55:39 +0000

    Initial commit

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div id="not-found">
+      <h1>Page not found</h1>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css"
+import { BrowserRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import App from "./components/App";
+import NotFound from "./components/NotFound";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
